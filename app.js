93e9ed889fa8e8c10aebf806d44a465dc9298628
HickAndrade <henrique_andrade2013@hotmail.com>
2022-09-22 11:44:51 -0300

    CORS, First Request Error, Custom Error Message, imports and packages.

---
+++ app.js
@@ -0,0 +1,33 @@
+'use strict'
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+
+app.use(morgan('dev'))
+app.use('/uploads', express.static('uploads'));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.use((req,res,next) => {
+    res.header('Access-Control-Allow-Origin', '*'); // Choose * from All or specify the URI to Access the Control.
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, content-type, Accept, Authorization'); // CORS Config
+    
+    if(req.method === 'OPTIONS'){ // Accept requests 
+        res.header('Access-Control-Allow-Methods','GET, POST, PUT, DELETE, PATCH');
+        return res.status(200).send({});
+    }
+    next();
+})
+
+app.use((req,res,next) =>{
+    const customError = new Error('Request not Found.')
+    customError.status = 404
+    next(customError);
+}) 
+
+app.use((customError, req,res,next) =>{
+    res.status(customError.status || 500);
+    return res.send({ msgError: customError.message })
+})
+
+module.exports = app;
